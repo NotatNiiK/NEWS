6168e71db689b42bb90800d6e3373fbcc4bd3d72
NotatNiiK <97485006+NotatNiiK@users.noreply.github.com>
2023-03-07 11:32:27 +0200

    Add files via upload

---
+++ src/components/NewsFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <footer class="footer">
+        <div class="footer__container">
+            <div class="footer__body">
+                <div class="footer__text">© Copyright 2023 | drile | All right reserved.</div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+
+.footer{
+    background: #04143c;
+    padding: 5px 0;
+    &__body{
+        padding: 15px 0;
+        text-align: center;
+    }
+    &__text{
+        font-size: 1rem;
+        color: #fff;
+        line-height: 120%;
+    }
+}
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="not-found">
+        <h1 class="not-found__title">Page not found</h1>
+    </div>
+</template>
+
+<style lang="scss">
+.not-found{
+    min-height: 100vh;
+    background: #04143c;
+
+    &__title{
+        text-transform: uppercase;
+        padding: 40px;
+        color: #fff;
+        font-size: 2.5rem;
+        color: #fff;
+        letter-spacing: 2px;
+        line-height: 120%;
+    }
+}
+</style>
